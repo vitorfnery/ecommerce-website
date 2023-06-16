1b299bc6ebf09bead4110bb607c7c17e09e23edb
Vítor F. Nery <vitorfelipenery@gmail.com>
2023-06-16 10:16:54 -0300

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Footer from "~containers/Footer";
+import Navbar from "~containers/Navbar";
+import Sidebar from "~containers/Sidebar";
+import Home from "~pages/Home";
+import NoPage from "~pages/NoPage";
+import ProductDetails from "~pages/ProductDetails";
+
 const App = () => {
   return (
-    <>
-      <div className="overflow-hidden">react app</div>
-    </>
+    <div className="overflow-hidden">
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+      <Sidebar />
+      <Footer />
+    </div>
   );
 };
 
---
+++ src/pages/NoPage/index.jsx
@@ -0,0 +1,5 @@
+const NoPage = () => {
+  return <div>404</div>;
+};
+
+export default NoPage;
